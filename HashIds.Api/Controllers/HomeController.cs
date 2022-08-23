157c8507f98c363e520e62bb658bea41a1ae6722
Ruchin Munjal <emailruchin@gmail.com>
2022-08-23 22:32:44 +0100

    Intial commit

---
+++ HashIds.Api/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using HashidsNet;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HashIds.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private readonly IHashids _hashIds;
+
+    private static readonly List<Person> People = new()
+    {
+        new Person { Id = 1, FirstName = "Ruchin", LastName = "Munjal" },
+        new Person { Id = 2, FirstName = "Shray", LastName = "Munjal" },
+        new Person { Id = 3, FirstName = "Parul", LastName = "Srivastava" },
+        new Person { Id = 4, FirstName = "Brinda", LastName = "Murali" },
+        new Person { Id = 5, FirstName = "Mia", LastName = "Munjal" }
+    };
+
+    public HomeController(IHashids hashIds)
+    {
+        _hashIds = hashIds;
+    }
+
+    [HttpGet]
+    public IActionResult GetDataForId(int id)
+    {
+        var mypeep=People.FirstOrDefault(x => x.Id == id);
+        if (mypeep==null)
+        {
+            return NotFound($"Couldn't find any person with the given id:{id}");
+        }
+
+        return Ok(mypeep);
+    }
+    [HttpGet("hashedId/{hashedId}")]
+    public IActionResult GetDataForId(string hashedId)
+    {
+        var id = _hashIds.Decode(hashedId)[0];
+        if (id <= 0)
+        {
+            return BadRequest($"Not a valid Id:{id}");
+        }
+        var mypeep=People.FirstOrDefault(x => x.Id == id);
+        if (mypeep==null)
+        {
+            return NotFound($"Couldn't find any person with the given id:{id}");
+        }
+
+        return Ok(mypeep);
+    }
+    [HttpGet("hashedId")]
+    public IActionResult GetAllDataEncoded()
+    {
+        var encodedPeople = (from person in People let encodedId = _hashIds.Encode(person.Id) 
+                                        select 
+                                            new PersonDto
+                                            {
+                                                Id = encodedId, FirstName = person.FirstName, 
+                                                LastName = person.LastName
+                                            }).ToList();
+
+        return Ok(encodedPeople);
+    }
+    
+    
+}
+
+public record PersonDto
+{
+    public string Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+public record Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
---
+++ HashIds.Api/Program.cs
@@ -0,0 +1,29 @@
+using HashidsNet;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<IHashids>(_ => new Hashids("duchiya",11));
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
